e4485d8bf532a6e303a93e6bc5a84487531dde12
Abdulrhman-Hassan <abdohassen100@gmail.com>
2026-04-13 02:32:30 +0200

    Add preemtive shortest job frist

--- OsClasses/Process.cs
+++ OsClasses/Process.cs
@@ -5,20 +5,26 @@
         private int _burstTime;
         private int _arrivalTime;
         private byte _priority;
+        private int _finishTime;
+        private int _waitingTime;
+        private int _turnaroundTime;
 
         public Process(int _burstTime) 
         {
             this._burstTime = _burstTime;
+            _waitingTime = -_burstTime;
         }
         public Process(int _burstTime, int _arrivalTime)
         {
             this._burstTime = _burstTime;
             this._arrivalTime = _arrivalTime;
+            _waitingTime = -_burstTime;
         }
         public Process(int _burstTime, byte _priority)
         {
             this._burstTime = _burstTime;
             this._priority = _priority;
+            _waitingTime = -_burstTime;
         }
         public int BurstTime
         {
@@ -33,7 +39,22 @@
         public byte Priority
         {
             get { return _priority; }
-            set { _arrivalTime= value; }
+            set { _priority = value; }
+        }
+        public int FinishTime
+        {
+            get { return _finishTime; }
+            set { _finishTime = value; }
+        }
+        public int WaitingTime
+        {
+            get { return _waitingTime; }
+            set { _waitingTime = value; }
+        }
+        public int TurnaroundTime
+        {
+            get { return _turnaroundTime; }
+            set { _turnaroundTime = value; }
         }
 
         // todo : build GetAverageWaitingTime method
---
+++ OsClasses/SJFpreemtive.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OsClasses
+{
+    public static class SJFpreemtive
+    {
+        private static readonly PriorityQueue<Process, (int primary, int secondary) > _processes = new PriorityQueue<Process, (int primary, int secondary)>();
+        private static int _pid;
+        private static int _count;
+
+        public static async Task Run(PriorityQueue<Process, int> processes,bool isLiveMode = true)
+        {
+            _pid = 0;
+            _count = 0;
+            Process currentProcess = processes.Dequeue();
+            while (processes.Count > 0 || _processes.Count > 0 || currentProcess != null)
+            {
+                if(currentProcess != null)
+                    currentProcess.BurstTime--;
+                if(isLiveMode)
+                    await Task.Delay(1000);
+                _count++;
+                if (currentProcess != null && currentProcess.BurstTime == 0)
+                {
+                    currentProcess.FinishTime = _count;
+                    currentProcess.WaitingTime += (currentProcess.FinishTime - currentProcess.ArrivalTime);
+                    currentProcess.TurnaroundTime = currentProcess.FinishTime - currentProcess.ArrivalTime;
+                    currentProcess = null;
+                }
+                while( processes.Count > 0 && processes.Peek().ArrivalTime == _count)
+                {
+                    if(currentProcess == null)
+                    {
+                        if (processes.Count > 0 && _processes.Count > 0)
+                        {
+                            if (processes.Peek().BurstTime < _processes.Peek().BurstTime)
+                            {
+                                currentProcess = processes.Dequeue();
+                            }
+                            else
+                            {
+                                currentProcess = _processes.Dequeue();
+                            }
+
+                        }
+                        else if (processes.Count > 0)
+                        {
+                            currentProcess = processes.Dequeue();
+                        }
+                        else if (_processes.Count > 0)
+                        {
+                            currentProcess = _processes.Dequeue();
+                        }
+                    }
+                    else if((processes.Peek().BurstTime < currentProcess.BurstTime ) && _processes.Count == 0)
+                    {
+                        _processes.Enqueue(currentProcess, (currentProcess.BurstTime, _pid++));
+                        currentProcess = processes.Dequeue();
+                    }
+                    else if(processes.Peek().BurstTime < currentProcess.BurstTime && processes.Peek().BurstTime < _processes.Peek().BurstTime)
+                    {
+                        _processes.Enqueue(currentProcess, (currentProcess.BurstTime, _pid++));
+                        currentProcess = processes.Dequeue();
+                    }
+                    else if(_processes.Peek().BurstTime < currentProcess.BurstTime && _processes.Peek().BurstTime < processes.Peek().BurstTime)
+                    {   
+                         currentProcess = _processes.Dequeue();
+                        _processes.Enqueue(currentProcess, (currentProcess.BurstTime, _pid++));
+                    }
+                    else if (processes.Count > 0)
+                    {
+                        int burstTime = processes.Peek().BurstTime;
+                        _processes.Enqueue(processes.Dequeue(), (burstTime, _pid++));
+                    }
+                }
+                if (currentProcess == null)
+                {   
+                    if (processes.Count > 0 && _processes.Count > 0)
+                    {
+                        if ((processes.Peek().BurstTime < _processes.Peek().BurstTime) && processes.Peek().ArrivalTime < _count)
+                        {
+                             currentProcess = processes.Dequeue();
+                        }
+                        else
+                        {
+                             currentProcess = _processes.Dequeue();
+                        }
+                    }
+                    else if (processes.Count > 0 && processes.Peek().ArrivalTime < _count)
+                    {
+                        currentProcess = processes.Dequeue();
+                    }
+                    else if (_processes.Count > 0)
+                    {
+                        currentProcess = _processes.Dequeue();
+                    }
+                }
+            }
+
+        }
+    }
+}
